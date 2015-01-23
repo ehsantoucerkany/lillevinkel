0077a3cb776006e9f07fd9c6650ff606d78769eb
iver56 <iver56@hotmail.com>
2015-01-23 13:37:42 +0100

    Implement absolute urls for images in product endpoint

--- webshop/views.py
+++ webshop/views.py
@@ -1,8 +1,20 @@
 from webshop.models import Product
 from rest_framework import viewsets
 from .serializers import ProductListSerializer
+from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().prefetch_related('variants')
-    serializer_class = ProductListSerializer
+    model = Product
+
+    def get_queryset(self):
+        return Product.objects.all().prefetch_related('variants')
+
+    def get_serializer_class(self):
+        return ProductListSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
