0e7ddfaeadbae8e1a477e36540e79ecc7122d8a5
iver56 <iver56@hotmail.com>
2015-01-23 11:43:37 +0100

    Implement __unicode__ for webshop models

--- webshop/models.py
+++ webshop/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.utils.encoding import smart_unicode
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
+
+    def __unicode__(self):
+        return smart_unicode("Brand: " + self.name)
 
 
 class Product(models.Model):
     name = models.CharField(blank=False, null=False, max_length=255)
     description = models.TextField(blank=False, null=False)
     brand = models.ForeignKey(to=Brand, blank=False, null=False)
+
+    def __unicode__(self):
+        return smart_unicode("Product: " + self.name)
 
 
 class ProductVariant(models.Model):
@@ -19,3 +26,10 @@ class ProductVariant(models.Model):
     quantity = models.IntegerField(blank=False, null=False)
 
     image = models.ImageField()
+
+    def __unicode__(self):
+        return smart_unicode(
+            "ProductVariant (product: " + self.product.name + ")"
+            + " (size: " + str(self.size) + ")"
+            + " (price: " + str(self.price) + ")"
+        )
